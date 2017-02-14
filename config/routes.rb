2e24070aea5bfbbb6daffb0903be58a9a9633eb2
shinmu13194 <aloneguy13194@gmail.com>
2017-02-14 16:16:23 +0700

    change layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   get 'page_home/contact'
 
+  root 'page_home#home'
+  get 'home',to:'page_home#home'
+  get 'help',to:'page_home#help'
+  get 'contact',to:'page_home#contact'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
